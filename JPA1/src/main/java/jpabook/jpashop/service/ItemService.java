eb0971eadeb254daee65209bc293f831bdd3620c
DESKTOP-3GBEFFK\DREAMS03 <03dreams@naver.com>
2024-07-07 23:06:30 +0900

    240707_spring 파일 복원 및 static 파일 추가

--- JPA1/src/main/java/jpabook/jpashop/service/ItemService.java
+++ JPA1/src/main/java/jpabook/jpashop/service/ItemService.java
@@ -1,32 +1,39 @@
 package jpabook.jpashop.service;
 
-import java.util.List;
-
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
 import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class ItemService {
-	
-	private final ItemRepository itemRepository;
-	
-	@Transactional
-	public void saveItem(Item item) {
-		itemRepository.save(item);
-	}
-	
-	// 조회
-	public List<Item> findItem() {
-		return itemRepository.findAll();
-	}
-	
-	public Item findOne(Long itemId) {
-		return itemRepository.findOne(itemId);
-	}
+
+    private final ItemRepository itemRepository;
+
+    @Transactional
+    public void saveItem(Item item) {
+        itemRepository.save(item);
+    }
+
+    @Transactional
+    public void updateItem(Long itemId, String name, int price, int stockQuantity) {
+        Item item = itemRepository.findOne(itemId);
+        item.setName(name);
+        item.setPrice(price);
+        item.setStockQuantity(stockQuantity);
+    }
+
+    public List<Item> findItems() {
+        return itemRepository.findAll();
+    }
+
+    public Item findOne(Long itemId) {
+        return itemRepository.findOne(itemId);
+    }
+
 }
