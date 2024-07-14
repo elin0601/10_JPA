39777a91e0ba1d89e6e9a47c03147638d924945b
DESKTOP-3GBEFFK\DREAMS03 <03dreams@naver.com>
2024-07-14 16:09:09 +0900

    240714_회원 수정 API Controller 작성

--- JPA1/src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ JPA1/src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -1,12 +1,15 @@
 package jpabook.jpashop.api;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.validation.Valid;
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.service.MemberService;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
@@ -46,6 +49,33 @@ public class MemberApiController {
 		member.setName(request.getName());
 		memberService.join(member);
 		return new CreateMemberResponse(member.getId());
+	}
+	
+	/**
+	 * 수정 API
+	 */
+	@PutMapping("api/v2/members/{id}")
+	public UpdateMemberResponse updateMemberV2(
+			@PathVariable("id") Long id, 
+			@RequestBody @Valid UpdateMemberReqeust request){
+		
+		memberService.update(id, request.getName());
+		
+		Member findMember = memberService.findOne(id);
+		
+		return new UpdateMemberResponse(findMember.getName(), findMember.getId());
+	}
+	
+	@Data
+	static class UpdateMemberReqeust{
+		private String name;
+	}
+	
+	@Data
+	@AllArgsConstructor
+	static class UpdateMemberResponse{
+		private String name;
+		private Long id;
 	}
 	
 	@Data
