9eed7d2e40c5d35c04818c2586459d0ead5d7ef7
DESKTOP-3GBEFFK\DREAMS03 <03dreams@naver.com>
2024-07-25 14:21:10 +0900

    240725_간단한 주문 조회 V1: 엔티티를 직접 노출 (build.gradle 오류 미해결 상태)

--- JPA1/src/main/java/jpabook/jpashop/JpashopApplication.java
+++ JPA1/src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -2,6 +2,10 @@ package jpabook.jpashop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.fasterxml.jackson.datatype.hibernate5.jakarta.Hibernate5JakartaModule;
+
 
 @SpringBootApplication
 public class JpashopApplication {
@@ -10,4 +14,17 @@ public class JpashopApplication {
 		SpringApplication.run(JpashopApplication.class, args);
 	}
 
+    @Bean
+    Hibernate5JakartaModule hibernate5Module() {	
+        return new Hibernate5JakartaModule();
+    }
+    
+	/* 다음과 같이 설정하면 강제로 지연 로딩 가능
+	 * @Bean 
+	 * Hibernate5Module hibernate5Module() { 
+	 * 	Hibernate5Module hibernate5Module = new Hibernate5Module(); // 강제 지연 로딩 설정
+	 * 	hibernate5Module.configure(Hibernate5Module.Feature.FORCE_LAZY_LOADING, true); 
+	 * 	return hibernate5Module; 
+	 * }
+	 */
 }
