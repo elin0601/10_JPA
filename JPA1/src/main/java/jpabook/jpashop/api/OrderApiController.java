6f458b49d774928db8f62430fcdedf13af1a60e6
DESKTOP-3GBEFFK\DREAMS03 <03dreams@naver.com>
2024-08-04 13:28:54 +0900

    240804_주문 조회 V3: 엔티티를 DTO로 변환 - 페치 조인 최적화

--- JPA1/src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ JPA1/src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import jpabook.jpashop.domain.Address;
@@ -71,6 +72,33 @@ public class OrderApiController {
 				.map(o -> new OrderDto(o))
 				.collect(Collectors.toList());
 		return collect;
+	}
+	
+	@GetMapping("/api/v3/orders")
+	public List<OrderDto> ordersV3() {
+		List<Order> orders = orderRepository.findAllWithItem();
+		List<OrderDto> result = orders.stream()
+				 .map(o -> new OrderDto(o))
+				 .collect(Collectors.toList());
+		return result;
+	}
+	
+	
+	/**
+	 * V3.1 엔티티를 조회해서 DTO로 변환 페이징 고려
+	 * - ToOne 관계만 우선 모두 페치 조인으로 최적화
+	 * - 컬렉션 관계는 hibernate.default_batch_fetch_size, @BatchSize로 최적화
+	 */
+	@GetMapping("/api/v3.1/orders")
+	public List<OrderDto> ordersV3_page(
+			@RequestParam(value = "offset", defaultValue = "0") int offset,
+			@RequestParam(value = "limit", defaultValue = "100") int limit) {
+		
+		List<Order> orders = orderRepository.findAllWithMemberDelivery(offset, limit);
+		
+		List<OrderDto> result = orders.stream().map(o -> new OrderDto(o)).collect(Collectors.toList());
+
+		return result;
 	}
 	
 	@Data
